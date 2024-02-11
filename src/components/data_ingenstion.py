987514bc85965a97f28a3bf83c4cc4680a6bd837
Sawan127 <sawanchaudhary127@gmail.com>
2024-02-11 16:48:01 +0530

    Model Trainer

--- src/components/data_ingenstion.py
+++ src/components/data_ingenstion.py
@@ -1,7 +1,8 @@
 import os
 import sys
-from src.exception import CustomException
+
 from src.logger import logging
+from src.exception import CustomException
 
 import pandas as pd 
 
@@ -10,6 +11,9 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -24,7 +28,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df = pd.read_csv('notebook\StudentsPerformance.csv')
+            df = pd.read_csv('notebook/StudentsPerformance.csv')
             logging.info("Read the dataset as the dataframe")
 
             os.makedirs(os.path.dirname(self.ingestion_config.test_data_path), exist_ok= True)
@@ -55,4 +59,7 @@ if __name__ == "__main__":
 
     data_transformation = DataTransformation()
     train_arr, test_arr, _= data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
             
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -13,8 +13,10 @@ from src.logger import logging
 from src.utils import save_object
 import os 
 
+@dataclass
 class DataTransformationConfig:
     preprocessor_obj_file_path = os.path.join('artifacts', 'prepocessor.pkl')
+
 
 class DataTransformation:
     def __init__(self):
@@ -36,7 +38,7 @@ class DataTransformation:
 
             num_pipeline = Pipeline(
                 steps= [
-                    ('imputer',SimpleImputer(strategy='medium')),
+                    ('imputer',SimpleImputer(strategy='median')),
                     ('scaler',StandardScaler())
                 ]
             )
@@ -45,7 +47,7 @@ class DataTransformation:
                 steps=[
                     ('imputer', SimpleImputer(strategy='most_frequent')),
                     ('one_hot_encoder',OneHotEncoder()),
-                    ('scaler',StandardScaler())
+                    ('scaler',StandardScaler(with_mean=False))
                 ]
             )
 
@@ -78,7 +80,7 @@ class DataTransformation:
 
             target_column_name = "math score"
 
-            numerical_columns = ['reading score','writing score']
+            numerical_columns = ['reading score','writing score']   
 
             input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
             target_feature_train_df = train_df[target_column_name]
@@ -86,10 +88,10 @@ class DataTransformation:
             input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
             target_feature_test_df = test_df[target_column_name]
 
-            logging.info('Applying preprocessing object on training dataframe and testing dataframe ')
+            logging.info(f'Applying preprocessing object on training dataframe and testing dataframe')
 
             input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr = preprocessing_obj.fit_transform(input_feature_test_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
 
             train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
             test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
